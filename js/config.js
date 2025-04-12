bf762e9099e2b4a7083643a61a9401f606127bd6
P Sibam <54404832+Sibu08@users.noreply.github.com>
2025-04-12 19:39:28 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_gyoSsRvhx', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '75a9obrn4vvlgse28hokok0de9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4cib8iqogtiffnp6opgjlnaovk', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
